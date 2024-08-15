20e1eaec79505a8dd0bbf6e2d4aa80f88f700db5
higorini <108697425+higorini@users.noreply.github.com>
2024-08-15 02:00:47 +0100

    feat: sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout/index.js'
+import Layout from './components/Layout'
 
 function App() {
   return (
